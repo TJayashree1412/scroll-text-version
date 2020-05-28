a2d387f4e8faa7de088bb2c0fa29509559359fb5
ravarsha <ravarsha@in.ibm.com>
2020-05-28 15:05:21 +0530

    authentication,grey theme and tab scss fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { PrivacyNoticeComponent } from './privacy-notice/privacy-notice.component';
 import { HomeComponent } from './home/home.component';
 import {CompensationhomeComponent} from './compensationhome/compensationhome.component';
+import {AuthenitcationService} from './authenticationService';
 
 
 const routes: Routes = [ 
   { path: '', component: LoginComponent },
 { path: 'Login', component: LoginComponent },
-{ path: 'privacyNotice',component: PrivacyNoticeComponent},
-{ path: 'Home', component: HomeComponent  },
-{ path: 'compHome', component: CompensationhomeComponent  },
+{ path: 'privacyNotice',component: PrivacyNoticeComponent,canActivate:[AuthenitcationService]},
+{ path: 'Home', component: HomeComponent,canActivate:[AuthenitcationService] },
+{ path: 'compHome', component: CompensationhomeComponent,canActivate:[AuthenitcationService]  },
 ];
 
 @NgModule({
