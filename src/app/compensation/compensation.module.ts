b1de6e9add0394fb6eb04915b9b82437e746e747
Benarji Vemuri <bvemur47@in.ibm.com>
2020-06-03 16:22:31 +0530

    create comp req page

--- src/app/compensation/compensation.module.ts
+++ src/app/compensation/compensation.module.ts
@@ -4,17 +4,39 @@ import { CommonModule } from '@angular/common';
 import { CompensationRoutingModule } from './compensation-routing.module';
 import { CompHomeComponent } from './comp-home/comp-home.component';
 import { MyCompRequestsComponent } from './my-comp-requests/my-comp-requests.component';
-import { AccordionModule } from 'carbon-components-angular';
+import { UIShellModule,GridModule, ListModule, TabsModule, TilesModule, InputModule, ButtonModule, ContentSwitcherModule, SideNavModule, RadioModule, CheckboxModule, ToggleModule, DropdownModule, FileUploaderModule, LoadingModule, DatePickerModule, ModalModule, PaginationModule, AccordionModule, SelectModule } from 'carbon-components-angular';
 import {Table, TableModule, TableModel, TableItem, TableHeaderItem } from 'carbon-components-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditCompRequestComponent } from './edit-comp-request/edit-comp-request.component';
-
+import { RaiseCompRequestComponent } from './raise-comp-request/raise-comp-request.component';
 @NgModule({
-  declarations: [CompHomeComponent, MyCompRequestsComponent, EditCompRequestComponent],
+  declarations: [CompHomeComponent, MyCompRequestsComponent, EditCompRequestComponent, RaiseCompRequestComponent],
   imports: [
     CommonModule,
     CompensationRoutingModule,
+    UIShellModule,
+    GridModule,
+    ListModule,
+    TabsModule,
+    TilesModule,
+    InputModule,
+    ButtonModule,
+    ContentSwitcherModule,
+    SideNavModule,
+    RadioModule,
+    CheckboxModule,
+    ToggleModule,
+    DropdownModule,
+    FileUploaderModule,
+    LoadingModule,
+    DatePickerModule,
+    ModalModule,
+    TableModule,
+    PaginationModule,
     AccordionModule,
-    TableModule
-  ]
+    SelectModule,
+    FormsModule,
+    ReactiveFormsModule
+    ]
 })
 export class CompensationModule { }
---
+++ src/app/compensation/raise-comp-request/raise-comp-request.component.ts
@@ -0,0 +1,120 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CompensationDTO } from 'src/app/ts-classes/compensationDTOs/CompensationDTO';
+import { CompensationService } from 'src/app/services/compensation.service';
+//import { CreateCompensation } from '../model/create-compensation';
+
+
+@Component({
+  selector: 'app-raise-comp-request',
+  templateUrl: './raise-comp-request.component.html',
+  styleUrls: ['./raise-comp-request.component.scss']
+})
+export class RaiseCompRequestComponent implements OnInit {
+  createComp: CompensationDTO;
+  statusCode : number;
+  errorMessage : string;
+  usstaymorethan89days=['Yes','No'];
+  empserial: string;
+  pmpseatid:string;
+  flog:boolean=false;
+  respData:{}=null;
+  formSubmitAttempt: boolean;
+
+  
+  EmpInfoForm = new FormGroup({
+  empserial:new FormControl(null,[Validators.required]),
+  pmpseatid:new FormControl(null,[Validators.required]),
+  empvisatype: new FormControl(null,[Validators.required]),
+  comptypselect:new FormControl(null,[Validators.required]),
+  countryselect:new FormControl(null,[Validators.required]),
+  HomeorHost:new FormControl(null,[Validators.required]),
+  us89daysstay:new FormControl('Yes')
+  
+  });
+  
+
+  constructor(private fb: FormBuilder,private router: Router, public compService: CompensationService) {}
+ 
+  ngOnInit(): void {
+    this.empserial = '';
+    this.pmpseatid ='';
+    
+    this.createCompensation();
+    }
+ 
+  async createCompensation(){
+    console.log('Satya')
+    this.compService.createCompensation().subscribe(
+     data =>{
+         this.createComp = data.body;
+       },
+      error => {
+        this.statusCode = error.status;
+        this.errorMessage = error.error;
+       } 
+     )
+   
+  }
+ 
+   onSubmit(){
+    this.formSubmitAttempt = true;
+  
+    console.log('Form submitted successfully');
+    console.log(this.EmpInfoForm);
+    
+      this.createComp = new CompensationDTO();
+      
+      this.createComp.empserial = this.EmpInfoForm.value.empserial;
+      this.createComp.pmpseatid = this.EmpInfoForm.value.pmpseatid;
+      this.createComp.countryCode=this.EmpInfoForm.value.countryselect.key;
+      this.createComp.compType=this.EmpInfoForm.value.comptypselect.key;
+      this.createComp.visaTyp=this.EmpInfoForm.value.empvisatype.key;
+   
+      this.compService.getEmployeeDetails(this.createComp).subscribe(resp =>{
+        this.statusCode = resp.status;
+        console.log('Success', this.statusCode);
+        console.log("Responce: "+JSON.stringify(resp));
+        this.respData = resp.body;
+        //this.createComp = resp.body
+        this.changeFlog();
+        //this.router.navigate(['/updateCompRequest']);
+      });
+    }
+   isFieldValid(field: string) {
+    return (!this.EmpInfoForm.get(field).valid && this.EmpInfoForm.get(field).touched) ||
+      (this.EmpInfoForm.get(field).untouched && this.formSubmitAttempt);
+  }
+ 
+
+ /* getEmployeeDetails(){
+    console.log('Benarji')
+  }*/
+  close(){
+    this.EmpInfoForm.reset();
+  this.formSubmitAttempt = false;
+  }
+
+  changeCompensationType(e) {
+    console.log(e.value)
+    this.EmpInfoForm.value.comptypselect.setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+  
+  changeHomeorHost(e) {
+    console.log(e.value)
+    /*this.EmpInfoForm.value.comptypselect.setValue(e.target.value, {
+      onlySelf: true
+    })*/
+  }
+
+  changeFlog(){
+    console.log('FLOG: '+this.flog);
+    this.flog=!this.flog;
+    console.log('FLOG: '+this.flog);
+  }
+  
+}
